431b58ec697867f15bdad9354fa7a4887727d957
Mikhail Trifonov <mick3fv@gmail.com>
2023-10-15 15:39:31 +0300

    SVO-24

--- Svobodny/Assets/CodeBase/Modules/Character/Animation/CharacterAnimatorController.cs
+++ Svobodny/Assets/CodeBase/Modules/Character/Animation/CharacterAnimatorController.cs
@@ -13,31 +13,26 @@ namespace CodeBase.Modules.Character.Animation
 
         private Animator _animator;
 
-        public void Contsruct(IInputService inputService, Animator animator, float walkSpeed, float sneakSpeed)
+        public void Construct(IInputService inputService, Animator animator, CharacterController controller, float walkSpeed, float sneakSpeed)
         {
             _inputService = inputService;
             _animator = animator;
+            _controller = controller;
             _walkSpeed = walkSpeed;
             _sneakSpeed = sneakSpeed;
         }
-
-        // Use this for initialization
-        void Start()
-        {
-            _controller = GetComponent<CharacterController>();
-        }
-
-        // Update is called once per frame
+        
         void Update()
         {
             var cameraInput = _inputService.CameraInput;
-            var movementInputLength = _inputService.MovementInput.sqrMagnitude;
             _animator.SetFloat(CharacterAnimatorVariables.MouseX, cameraInput.x);
             _animator.SetFloat(CharacterAnimatorVariables.MouseY, cameraInput.y);
             _animator.SetFloat(CharacterAnimatorVariables.Speed, _controller.velocity.sqrMagnitude);
             _animator.SetFloat(CharacterAnimatorVariables.WalkSpeed, _walkSpeed);
             _animator.SetFloat(CharacterAnimatorVariables.SneakSpeed, _sneakSpeed);
             _animator.SetBool(CharacterAnimatorVariables.IsSneaking, _inputService.IsSneakButtonDown());
+            _animator.SetFloat(CharacterAnimatorVariables.MovementX, _inputService.MovementInput.x);
+            _animator.SetFloat(CharacterAnimatorVariables.MovementY, _inputService.MovementInput.y);
         }
     }
 }
