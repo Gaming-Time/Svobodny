a029779bb84e20b655eb938f7b1c5bff5983479a
Mikhail Trifonov <mick3fv@gmail.com>
2024-01-29 12:26:33 +0300

    SVO-41 - Пофиксил

--- Svobodny/Assets/CodeBase/Infrastructure/Logic/UsableObjects/UsableObject.cs
+++ Svobodny/Assets/CodeBase/Infrastructure/Logic/UsableObjects/UsableObject.cs
@@ -10,28 +10,35 @@ namespace CodeBase.Infrastructure.Logic.UsableObjects
     {
         protected abstract IInputService InputService { get; set; }
         private bool _isRequested;
-        
+
 
         private void Update()
         {
             OnUpdate();
         }
 
-        private void OnTriggerStay(Collider other)
+        private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.layer != LayerMask.NameToLayer("Character Trigger"))
                 return;
-            
-            if(_isRequested)
-                Use();
+
+            _isRequested = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.layer != LayerMask.NameToLayer("Character Trigger"))
+                return;
+
+            _isRequested = false;
         }
 
         public abstract void Use();
 
         protected virtual void OnUpdate()
         {
-            _isRequested = InputService.IsUseButtonDown();
-            Debug.Log(_isRequested);
+            if (_isRequested && InputService.IsUseButtonDown())
+                Use();
         }
     }
 }
