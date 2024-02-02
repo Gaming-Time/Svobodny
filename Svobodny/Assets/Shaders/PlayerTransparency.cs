5b2ccb5632371baff82b6e426ad525ce340ebdf7
Mikhail Trifonov <mick3fv@gmail.com>
2024-02-02 13:15:51 +0300

    123

--- Svobodny/Assets/Shaders/PlayerTransparency.cs
+++ Svobodny/Assets/Shaders/PlayerTransparency.cs
@@ -1,21 +1,19 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerTransparency : MonoBehaviour
 {
-    public Material Material;
-    public LayerMask LayerMask;
+    [FormerlySerializedAs("LayerMask")] public LayerMask WallLayerMask;
+    [SerializeField] private LayerMask characterLayerMask;
     public Camera Camera;
     public static int PosId = Shader.PropertyToID("_PlayerPos");
     public static int SizeId = Shader.PropertyToID("_Size");
 
+    private Material[] _wallMaterials;
+
     public void Construct(Camera camera)
     {
         Camera = camera;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
     }
 
     // Update is called once per frame
@@ -23,24 +21,39 @@ public class PlayerTransparency : MonoBehaviour
     {
         var dir = Camera.transform.position - transform.position;
         var ray = new Ray(transform.position, dir.normalized);
-        if (Physics.Raycast(ray, 3000, LayerMask))
+        if (Physics.Raycast(ray, 3000, WallLayerMask))
         {
             RaycastHit hit;
-            Physics.Raycast(ray, out hit);
-            Material = hit.collider.gameObject.GetComponent<Renderer>().material;
+            Physics.Raycast(ray, out hit, 3000, WallLayerMask);
+            _wallMaterials = hit.collider.gameObject.GetComponent<Renderer>()?.materials;
 
-            if (Material != null)
-                Material.SetFloat(SizeId, 1);
+            if (_wallMaterials != null)
+            {
+                foreach (var wallMaterial in _wallMaterials)
+                {
+                    wallMaterial.SetFloat(SizeId, 1);
+                }
+            }
         }
         else
         {
-            if (Material != null)
-                Material.SetFloat(SizeId, 0);
+            if (_wallMaterials != null)
+            {
+                foreach (var wallMaterial in _wallMaterials)
+                {
+                    wallMaterial.SetFloat(SizeId, 0);
+                }
+            }
         }
 
         var view = Camera.WorldToViewportPoint(transform.position); //Получение координат
 
-        if (Material != null)
-            Material.SetVector(PosId, view);
+        if (_wallMaterials != null)
+        {
+            foreach (var wallMaterial in _wallMaterials)
+            {
+                wallMaterial.SetVector(PosId, view);
+            }
+        }
     }
 }
