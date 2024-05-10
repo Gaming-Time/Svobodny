f98b8716b78546fe42150e4faa2b7ddd8822b8da
Mikhail Trifonov <mick3fv@gmail.com>
2024-05-10 13:25:37 +0300

    SVO-50 - Убрал лишний код из аниматора руки

--- Svobodny/Assets/CodeBase/Modules/Character/Arm/ArmAnimatorController.cs
+++ Svobodny/Assets/CodeBase/Modules/Character/Arm/ArmAnimatorController.cs
@@ -16,27 +16,6 @@ namespace CodeBase.Modules.Character.Arm
             _inputService = inputService;
         }
 
-        private void Update()
-        {
-            /*var cameraInput = _inputService.CameraInput;
-
-            var angle = Vector2.SignedAngle(Vector2.right, cameraInput);
-            
-            Debug.Log($"Angle:{angle}, sin:{Mathf.Sin(angle)}");
-            
-            animator.SetFloat(AnimatorVariables.Arm.MouseX, cameraInput.x);
-            animator.SetFloat(AnimatorVariables.Arm.MouseY, cameraInput.y);*/
-
-            /*transform.localPosition = angle switch
-            {
-                > -45 and < 45 => new Vector3(0.086f, 0.431f, -0.375f),
-                >= 45 and <= 135 => new Vector3(0.139f, 0.08f, 0.252f),
-                <= -45 and >= -135 => new Vector3(-0.157f,0.431f,-0.375f),
-                < -135 or > 135 => new Vector3(-0.112f, -0.011f, 0.228f),
-                _ => transform.localPosition
-            };*/
-        }
-
         public void SetMouseVariables()
         {
             var cameraInput = _inputService.CameraInput;
